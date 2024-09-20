75e7fa2cbd653796de12f110f53afe89fb5af7d0
Jose Miguel <josemiguel.lml@gmail.com>
2024-09-19 23:43:15 -0600

    Improve component login

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -13,64 +13,7 @@ import TextField from './TextField';
 import Typography from "@mui/material/Typography";
 import Button from "./Button";
 import { styled } from '@mui/system';
-
-/* const IOSSwitch = styled((props) => (
-  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} /> ))(({ theme }) => ({
-  width: 42,
-  height: 26,
-  padding: 0,
-  '& .MuiSwitch-switchBase': {
-    padding: 0,
-    margin: 2,
-    transitionDuration: '300ms',
-    '&.Mui-checked': {
-      transform: 'translateX(16px)',
-      color: '#fff',
-      '& + .MuiSwitch-track': {
-        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
-        opacity: 1,
-        border: 0,
-      },
-      '&.Mui-disabled + .MuiSwitch-track': {
-        opacity: 0.5,
-      },
-    },
-    '&.Mui-focusVisible .MuiSwitch-thumb': {
-      color: '#33cf4d',
-      border: '6px solid #fff',
-    },
-    '&.Mui-disabled .MuiSwitch-thumb': {
-      color:
-        theme.palette.mode === 'light'
-          ? theme.palette.grey[100]
-          : theme.palette.grey[600],
-    },
-    '&.Mui-disabled + .MuiSwitch-track': {
-      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
-    },
-  },
-  '& .MuiSwitch-thumb': {
-    boxSizing: 'border-box',
-    width: 22,
-    height: 22,
-  },
-  '& .MuiSwitch-track': {
-    borderRadius: 26 / 2,
-    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#efb810',
-    opacity: 1,
-    transition: theme.transitions.create(['background-color'], {
-      duration: 500,
-    }),
-  },
-})); 
-
-<FormControlLabel
-  control={<IOSSwitch sx={{ m: 1, }}  defaultChecked />}
-  onClick={handleChange}
-/> 
-
-*/
-
+  
 const style = {
   position: 'absolute',
   top: '50%',
@@ -88,15 +31,23 @@ export default function Login({
   handleClose,
 }) {
 
+  /* 
+    TODO:
+      1. Verify password: regular expression
+      2. Verify email: regular expression
+      3. Send Data Login / Register to Backend
+      4. Show password
+  */
+
   const [rotation, setRotation] = useState(0);
-  // const [showPassword, setShowPassword] = useState(false);
   const [changeForm, setChangeForm] = useState(true);
   const handleChangeForm = () => {
     setRotation(rotation + 180);
     setChangeForm(!changeForm);
   };
-
+  
   // Data Login
+  // const [showPassword, setShowPassword] = useState(false);
   const [login, setLogin] = useState({email: '', password: ''});
   const handleLogin=(e) => setLogin({ ...login, [e.target.name]: e.target.value });
   const handleSubmitLogin = (event) => {
@@ -104,7 +55,7 @@ export default function Login({
     console.log('Formulario enviado:', login);
   };
   
-  //Data Register
+  // Data Register
   const [register, setRegister] = useState({name: '', lastname: '', email: '', password: ''});
   const handleRegister=(e) => setRegister({ ...register, [e.target.name]: e.target.value });
   const handleSubmitRegister = (event) => {
@@ -115,26 +66,29 @@ export default function Login({
   return (
     <div>
       <Modal open={open} onClose={handleClose} sx={{color: 'inherit'}}>
-        <Box color='inherit' sx={{ ...style, width: '50vh', height: '70vh', maxHeight: '70vh' }}>
+        <Box color='inherit' sx={{ ...style, width: '60vh', height: '70vh', maxHeight: '80vh' }}>
           
           <Grid container justifyContent='center' sx={{pt: 2, }} >
             <FormGroup>
               <div style={{display: 'flex', alignItems: 'center', justifyContent:'stretch', width: '40vh', gap: 10}}>
-                <IconButton onClick={handleChangeForm} sx={{'&:focus': { outline: 'none', }, transition: 'transform 0.5s', transform: `rotate(${rotation}deg)`,}} >
-                  <SyncIcon sx={{color: '#efb810' }}/>
-                </IconButton>
                 {changeForm ? 
-                  <Typography variant='h5'> Iniciar Sesión</Typography> 
+                  <Grid>
+                    <Typography variant='h5'> Iniciar Sesión</Typography> 
+                  </Grid>
                 : 
-                  <Typography variant='h5'> Nueva Cuenta </Typography> 
+                  <Grid>
+                    <Typography variant='h5'> Nueva Cuenta </Typography> 
+                  </Grid>
                 }
               </div>
+              <Typography variant='body'>¡Bienvenido! Ingresa los siguientes datos.</Typography> 
             </FormGroup>
           </Grid>
           
           <Grid container justifyContent='center'>
             {changeForm ?
-            <>
+            <> 
+              {/* Login */}
               <Grid container direction='column' alignContent='center' sx={{pt: 5, pb: 3, gap: 3, }}>
                 <TextField 
                   type='email' 
@@ -143,29 +97,29 @@ export default function Login({
                   value={login.email} 
                   onChange={handleLogin}
                   size='small'
-                  sx={{width: '40vh'}}/>
+                  sx={{width: '50vh'}}
+                />
                 <TextField 
-                  type='password' 
+                  type='password'
                   label='Contraseña' 
                   name='password' 
                   value={login.password} 
                   onChange={handleLogin}
                   size='small'
-                  sx={{width: '40vh'}}/>
-              </Grid>
+                  sx={{width: '50vh'}}
+                />
+                <Typography variant='caption' textAlign={'right'} sx={{textDecoration: 'underline', cursor: 'pointer'}}> 
+                  Restablecer contraseña 
+                </Typography>
+              </Grid>           
 
               <Grid container direction='column' alignItems='center'  sx={{ gap: 2  }}>
-                <Button type='submit' title='Iniciar Sesión' onClick={handleSubmitLogin} sx={{width: '25vh'}}/>
-                <Typography variant='body' sx={{textDecoration: 'underline', cursor: 'pointer'}}> Olvide mi contraseña </Typography>
-                <a onClick={handleChangeForm}> 
-                  <Typography variant='body' sx={{textDecoration: 'underline', cursor: 'pointer'}}> 
-                    Crear nueva cuenta 
-                  </Typography> 
-                </a> 
+                <Button type='submit' title='Ingresar' onClick={handleSubmitLogin} sx={{width: '50vh'}}/>
+                <Button type='submit' title='Registrarse' onClick={handleChangeForm} sx={{width: '50vh'}}/>
               </Grid>
             </> 
             : 
-            <>
+            <> {/* New Account */}
               <Grid container direction='column'  alignContent='center' sx={{pt: 3, pb: 3, gap: 2, }}>
                 <TextField 
                   type='text' 
@@ -174,7 +128,8 @@ export default function Login({
                   value={register.name} 
                   onChange={handleRegister}
                   size='small'
-                  sx={{width: '40vh'}}/>
+                  sx={{width: '50vh'}}
+                />
                 <TextField 
                   type='text' 
                   label='Apellidos' 
@@ -182,7 +137,8 @@ export default function Login({
                   value={register.lastname} 
                   onChange={handleRegister}
                   size='small'
-                  sx={{width: '40vh'}}/>
+                  sx={{width: '50vh'}}
+                />
                 <TextField 
                   type='email' 
                   label='Email' 
@@ -190,7 +146,8 @@ export default function Login({
                   value={register.email} 
                   onChange={handleRegister}
                   size='small'
-                  sx={{width: '40vh'}}/>
+                  sx={{width: '50vh'}}
+                />
                 <TextField 
                   type='password' 
                   label='Contraseña' 
@@ -198,10 +155,17 @@ export default function Login({
                   value={register.password} 
                   onChange={handleRegister}
                   size='small'
-                  sx={{width: '40vh'}}/>
+                  sx={{width: '50vh'}}
+                />
               </Grid>
+
               <Grid container direction='column' alignContent='center' alignItems='center' sx={{width: 200, gap: 2}}>  
-                <Button type='submit' title='Registrarse' onClick={handleSubmitRegister} sx={{width: '25vh'}}/>
+                <Button 
+                  type='submit' 
+                  title='Registrarse' 
+                  onClick={handleSubmitRegister} 
+                  sx={{width: '50vh'}}
+                />
                 <a onClick={handleChangeForm}> 
                   <Typography variant='body' sx={{textDecoration: 'underline', cursor: 'pointer'}}> 
                     ¿Ya tienes cuenta? 
